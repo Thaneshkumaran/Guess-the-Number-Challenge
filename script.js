d2121263aaca9dd4f1b4e869150536f9bb46d6fd
Thaneshkumaran R <thaneshr42@gmail.com>
2024-10-24 16:12:00 +0530

    to change

--- script.js
+++ script.js
@@ -1,28 +1,44 @@
-let randomNumber = Math.floor(Math.random() * 100) + 1;
+let randomNumber = Math.floor(Math.random() * 10) + 1;
 let guessCount = 0;
+const maxAttempts = 3;
+let attempts = 0;
 
 function checkGuess() {
-    const userGuess = document.getElementById('userGuess').value;
     const message = document.getElementById('message');
     const tries = document.getElementById('tries');
+
+    const userGuess = Number(document.getElementById('userGuess').value); 
     guessCount++;
 
     if (userGuess == randomNumber) {
         message.textContent = 'Congratulations! You guessed the number!';
         message.style.color = 'green';
         tries.textContent = `It took you ${guessCount} tries.`;
-    } else if (userGuess < randomNumber) {
+        return; 
+    } else if (userGuess > 0 && userGuess < randomNumber) {
         message.textContent = 'Too low! Try again.';
         message.style.color = 'red';
-    } else if (userGuess > randomNumber) {
+    } else if (userGuess < 11 && userGuess > randomNumber) {
         message.textContent = 'Too high! Try again.';
+        message.style.color = 'red';
+    } else {
+        message.textContent = 'Number is not valid. Please enter a number between 1 and 10.';
+        message.style.color = 'red';
+    }
+
+    attempts++;
+
+    
+    if (attempts >= maxAttempts) {
+        message.textContent = 'You took too long. Please try again!';
         message.style.color = 'red';
     }
 }
 
 function resetGame() {
-    randomNumber = Math.floor(Math.random() * 100) + 1;
+    randomNumber = Math.floor(Math.random() * 10) + 1;
     guessCount = 0;
+    attempts = 0; 
     document.getElementById('message').textContent = '';
     document.getElementById('tries').textContent = '';
     document.getElementById('userGuess').value = '';
